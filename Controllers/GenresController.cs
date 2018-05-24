111915863d0b0c74f3ef116e7b906db08089949d
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2018-05-24 12:41:55 -0500

    Added files

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Spin.Services.Interfaces;
+
+namespace Spin.Controllers
+{
+    [RoutePrefix("Genres")]
+    public class GenresController : Controller
+    {
+        protected readonly IAlbum _genreService;
+
+        public GenresController(IAlbum genreService)
+        {
+            _genreService = genreService;
+        }
+
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [Route("Albums/{genreId}", Name = "AllAlbumsByGenre")]
+        public ActionResult Albums(int genreId)
+        {
+            var model = _genreService.GetAlbumsByGenreId(genreId);
+
+            return View(model);
+        }
+    }
+}
---
+++ ViewModels/Artists/ArtistsDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Spin.Models;
+
+namespace Spin.ViewModels.Artists
+{
+    public class ArtistsDetailsViewModel
+    {
+        public Artist Artist { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
